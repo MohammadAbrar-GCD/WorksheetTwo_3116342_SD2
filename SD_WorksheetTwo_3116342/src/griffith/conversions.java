244f1e8f1c0b8ba3f3d00bbf57ad2d6b96261813
Mohammad Abrar Farooqui <mohammmadabrar.farooqui@student.griffith.ie>
2025-03-06 23:33:58 +0000

    added the all stub methods to conversions.java

--- SD_WorksheetTwo_3116342/src/griffith/conversions.java
+++ SD_WorksheetTwo_3116342/src/griffith/conversions.java
@@ -1,5 +1,24 @@
 package griffith;
 
 public class conversions {
+	
+	  public double euroToDollar(double euro) {
+	        return 0.0; // stub method
+	    }
+	  
+	  public double dollarToEuro(double dollar) {
+	        return 0.0; // stub method
+	    }
 
+	  public int stringToInteger(String val) {
+	        return 0; // stub method
+	    }
+
+	    public String integerToString(int val) {
+	        return ""; // stub method
+	    }
+
+	    public String switchCase(String input) {
+	        return ""; // stub method
+	    }
 }
